a57120dc84d4ec07bde01f0fe2700ee0d5b588a0
Sergio Ortiz <sergiohoo@gmail.com>
2021-05-21 00:49:43 +0000

    Listo boton para cancelar el wizard

--- Employees/webapp/controller/CreateEmployee.controller.js
+++ Employees/webapp/controller/CreateEmployee.controller.js
@@ -1,17 +1,32 @@
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
+        "sap/ui/core/mvc/Controller",
+        "sap/m/MessageBox"
 	],
 	/**
      * Controlador de la
      * vista para ver los empleados
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.m.MessageBox} MessageBox
      */
-	function (Controller) {
+	function (Controller, MessageBox) {
 		"use strict";
 
 		return Controller.extend("hr.Employees.controller.CreateEmployee", {
 			onInit: function () {
 
-			}
+            },
+            
+            onCancel: function () {
+                let oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+                MessageBox.confirm(oResourceBundle.getText("confirmCancelCreateEmployee"), {
+                    onClose: function (oAction) {
+                        if (oAction === "OK") {
+                            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                            oRouter.navTo("RouteLaunchpad");
+                        }
+                    }.bind(this)
+                });
+            }
 		});
 	});
